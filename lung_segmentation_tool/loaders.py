b20d18c219db2745d4fb9568a1a2d9b1d7888cb3
Tristan Penman <tristan@tristanpenman.com>
2018-11-18 10:57:27 +1100

    Remove redundant functions from loaders.py

--- lung_segmentation_tool/loaders.py
+++ lung_segmentation_tool/loaders.py
@@ -6,22 +6,6 @@ import pydicom
 import SimpleITK as sitk
 
 
-def get_pixels_in_hounsfield_units(slices):
-    image = np.stack([s.pixel_array for s in slices])
-    image = image.astype(np.int16)
-    image[image == -2000] = 0
-
-    for slice_number in range(len(slices)):
-        intercept = slices[slice_number].RescaleIntercept
-        slope = slices[slice_number].RescaleSlope
-        if slope != 1:
-            image[slice_number] = slope * image[slice_number].astype(np.float64)
-            image[slice_number] = image[slice_number].astype(np.int16)
-        image[slice_number] += np.int16(intercept)
-
-    return np.array(image, dtype=np.int16)
-
-
 def load_dicom_files(scan_path):
     # Read all slices in the given directory
     slices = [pydicom.read_file(os.path.join(scan_path, s)) for s in os.listdir(scan_path)]
@@ -43,12 +27,3 @@ def load_mhd_file(scan_path):
     img_array = sitk.GetArrayFromImage(itk_img)
     size = itk_img.GetSize()
     return img_array, np.array(itk_img.GetSpacing())[::-1], size[1], size[0]
-
-
-def normalize(image):
-    min_bound = -1000.0
-    max_bound = 400.0
-    image = (image - min_bound) / (max_bound - min_bound)
-    image[image > 1] = 1.
-    image[image < 0] = 0.
-    return image
